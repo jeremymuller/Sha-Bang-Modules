bc8daffb0352ca987571e82c14500eabf1448e88
Jeremy <jeremymuller@users.noreply.github.com>
2020-10-01 15:49:12 -0400

    added DC offset

--- src/QubitCrusher.cpp
+++ src/QubitCrusher.cpp
@@ -16,25 +16,29 @@ struct QubitCrusher : Module {
 		NUM_LIGHTS
 	};
 
+    dsp::BiquadFilter hip; 
+
     QubitCrusher() {
         config(NUM_PARAMS, NUM_INPUTS, NUM_OUTPUTS, NUM_LIGHTS);
     }
 
     void process(const ProcessArgs &args) override {
+        // this works for the most part but I may try a different way
         int channels = std::max(inputs[MAIN_INPUT].getChannels(), 1);
 
         for (int c = 0; c < channels; c++) {
             float in = inputs[MAIN_INPUT].getVoltage(c);
             in = rescale(in, -5.0, 5.0, 0.0, 1.0);
-            float scl = std::pow(2, 2.0) - 1;
+            float scl = std::pow(2, 4.0) - 1;
             in = static_cast<int>(in * scl);
             in /= scl;
             in = rescale(in, 0.0, 1.0, -5.0, 5.0);
 
-            // TODO: add DC offset
-            // rack::dsp::BiquadFilter hip = rack::dsp::BiquadFilter();
-            // hip.setParamters(HIGHPASS_1POLE, 5.0);
-            outputs[MAIN_OUTPUT].setVoltage(in, c);
+            // add DC offset
+            hip.setParameters(dsp::BiquadFilter::HIGHPASS_1POLE, 5.0 / args.sampleRate, 0.0, 0.0);
+            float out = hip.process(in);
+
+            outputs[MAIN_OUTPUT].setVoltage(out, c);
         }
         outputs[MAIN_OUTPUT].setChannels(channels);
     }
